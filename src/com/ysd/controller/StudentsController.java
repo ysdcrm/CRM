2d60749c1a7dca51650ac45a659baa60beda8641
dell <dell@DESKTOP-2S7NDM6>
2019-05-15 11:00:45 +0800

    胡科研提交

--- src/com/ysd/controller/StudentsController.java
+++ src/com/ysd/controller/StudentsController.java
@@ -1,8 +1,55 @@
 package com.ysd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ysd.entity.Fenye;
+import com.ysd.entity.Students;
+import com.ysd.service.StudentsService;
 
 @Controller
 public class StudentsController {
+	@Autowired
+	private StudentsService studentsService;
+	@Autowired
+	private Fenye fenye;
+	
+	@RequestMapping(value="/showStu",method=RequestMethod.POST)
+	@ResponseBody
+	public Fenye showStu(Integer page,Integer rows) {
+		fenye.setPage((page-1)*rows);
+		fenye.setPageSize(rows);
+		Fenye selectAllStu = studentsService.selectAllStu(fenye);
+		return selectAllStu;
+		
+	}
+	/*
+	 * @RequestMapping(value="/addStu",method=RequestMethod.POST)
+	 * 
+	 * @ResponseBody public Integer addStu(Students students) {
+	 * 
+	 * Integer i = studentsService.addStu(students); return i;
+	 * 
+	 * }
+	 * 
+	 * @RequestMapping(value="/addStu",method=RequestMethod.POST)
+	 * 
+	 * @ResponseBody public Integer updatetu(Students students) {
+	 * 
+	 * Integer i = studentsService.updateStu(students); return i;
+	 * 
+	 * }
+	 * 
+	 * @RequestMapping(value="/delStu",method=RequestMethod.POST)
+	 * 
+	 * @ResponseBody public Integer delStu(Integer student_id) {
+	 * 
+	 * Integer i = studentsService.delStu(student_id); return i;
+	 * 
+	 * }
+	 */
 
 }
--- src/com/ysd/dao/StudentsMapper.java
+++ src/com/ysd/dao/StudentsMapper.java
@@ -1,5 +1,21 @@
 package com.ysd.dao;
 
+import java.util.List;
+
+import com.ysd.entity.Fenye;
+import com.ysd.entity.Students;
+
 public interface StudentsMapper {
+	
+	Integer selectCountStu(Fenye fenye);
+	
+    List<Students> selectAllStu(Fenye fenye);
+	
+	Integer addStu(Students students);
+	
+	Integer updateStu(Students students);
+	
+	Integer delStu(Integer student_id);
+	
 
 }
